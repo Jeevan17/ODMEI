92ec1e4ab3b20901866d303afa316d0e13169372
jeevan17 <jeevan17>
2018-01-14 18:46:50 +0530

    Bootstrap(index.php)

--- index.php
+++ index.php
@@ -31,24 +31,42 @@
 			<html>
 				<head>
 					<title>CBIT MANAGEMENT SYSTEM</title>
-					<link rel='stylesheet' href='style.css'>
+
+					<!--BootStrap-->
+						<meta charset='utf-8'>
+						<meta name='viewport' content='width=device-width, initial-scale=1'>
+  						<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css'>
+  						<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+  						<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js'></script>
+ 						 <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js'></script>
+
+ 					<!--BootStrap End-->
+
+
+					<!--link rel='stylesheet' href='style.css'-->
 				</head>
 				<body>
-					<center>
-					<br><br><br><br><br><br>
-						<h1  style='font-size: 30px;color: #05A8AA;'>
-								Chaitanya Bharathi Institute of Technology
-							</h1>
-						<form method='POST' action='index.php' >
-							<h3>USERNAME	
-							<input type='text' name='username' required class='log_text' placeholder='Enter Username'><br><br>
-							PASSWORD
-							<input type='password' name='password' required class='log_text' placeholder='Enter Password'><br>
-							<input type='submit' value='Login' name='submit' class='sub_btn'></h3>
-						</form>
-					</center>
-				</body>
-			</html>
+					<div class='container mt-5 ml-3 mr-5 pr-5'>
+							<div class='row'>
+								<div class='col-sm-12'>
+									<h2 class='text-info'>
+											Chaitanya Bharathi Institute of Technology
+									</h2>
+								</div>
+							</div>
+								<form method='POST' action='index.php'>
+									<div class='form-group'>
+									    <label for='usr'>Username</label>
+									    <input type='text' class='form-control' id='usr' placeholder='Enter Username' name='username' required>
+									</div>
+									<div class='form-group'>
+									    <label for='pwd'>Password</label>
+									    <input type='password' class='form-control' id='pwd' placeholder='Enter Password' name='password' required>
+									</div>
+
+									<input type='submit' value='Login' name='submit' class='btn btn-outline-primary btn-block'></h4>
+								</form>
+					</div>
 		";
 				
 		if(isset($_POST["submit"]))
@@ -65,8 +83,13 @@
 				$retval = mysqli_query($conn, $sql);
 				if(mysqli_affected_rows($conn)==0)
 				{
-					echo "<script>alert('Invalid user credentials!! ')</script>";
-					//die('Could not get data: ' . mysqli_error());
+					
+					echo "
+						<div class='alert alert-danger'>
+							<strong>Invalid User Credentials!</strong>
+						</div>
+						";
+					//echo "<script>alert('Invalid user credentials!! ')</script>";
 				}
 				else
 				{
@@ -110,4 +133,9 @@
 				}	
 			}
 		}
+
+	echo "
+		</body>
+		</html>
+	"
 ?>
--- principal.php
+++ principal.php
@@ -17,6 +17,20 @@
 		<html>
 				<head>
 					<title>Principal CBIT</title>
+
+
+					<!--BootStrap-->
+						<meta charset='utf-8'>
+						<meta name='viewport' content='width=device-width, initial-scale=1'>
+  						<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css'>
+  						<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+  						<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js'></script>
+ 						 <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js'></script>
+
+ 					<!--BootStrap End-->
+
+
+
 					<link rel='stylesheet' href='style.css'>
 				</head>
 				<body>
@@ -43,7 +57,7 @@
 	{
 		if($_POST['rollno']!=null)
 		{
-			session_start();
+			//session_start();
 			if(!isset($_SESSION['principal'])){
 				echo "<script language='javascript'>window.location='index.php';</script>";
 			}
