135fed8f70d74f1a971584aaa7e7296d429b5ab0
Jeevan17 <gandla.jeevan17@gmail.com>
2018-01-30 15:07:44 +0530

    Add files via upload

---
+++ Student/Placement.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Placement</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    </head>
+
+    <body>
+      <div class="container">
+        
+        <div class='row pt-1'>
+          <div class='col-sm-2'>
+          </div>
+          <div class='col-sm-8'>
+            <img src='../images/header.jpg' class='img-fluid'>
+          </div>
+          <div class='col-sm-1'>
+          </div>
+          <div class='col-sm-1 pt-3 pl-5'>
+            <a href='../index.php' class='btn btn-danger btn-sm'>Logout</a>
+          </div>
+        </div>
+        <div class='container pt-3'>
+          <div class='row'>
+            <div class='col-sm-12'>
+              <ul class='nav nav-tabs nav-justified'>
+                <li class='nav-item'>
+                  <a class='nav-link active ' href='Placement.php'>Home</a>
+                </li>
+                <li class='nav-item'>
+                  <a class='nav-link ' href='Company_details.php'>Company Details</a>
+                </li>
+                <li class='nav-item'>
+                  <a class='nav-link ' href='Student_details.php'>Student wise Details</a>
+                </li>
+              </ul>
+              <div class='tab-content'>
+                <div class='container tab-pane active'><br>
+                  <div class='row'>
+                    <div class='col-sm-6'>
+                      <h3>Placement Details</h3>
+                    </div>
+                    <div class='col-sm-3'>
+                    </div>
+                    <div class='col-sm-3'>
+                      <b>Batch: </b><mark>2017-2018</mark> 
+                    </div>
+                  <div class='table-responsive'>
+                    <table class='table table-bordered table-hover'>
+                      <thead>
+                        <tr>
+                          <th>Company Name</th>
+                          <th>CSE</th>
+                          <th>ECE</th>
+                          <th>IT</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th>JPMC</th>
+                          <td>15</td>
+                          <td>-</td>
+                          <td>5</td>
+                        </tr>
+                        <tr>
+                          <th>Oracle</th>
+                          <td>2</td>
+                          <td>0</td>
+                          <td>2</td>
+                        </tr>
+                        <tr>
+                          <th>BOA</th>
+                          <td>12</td>
+                          <td>3</td>
+                          <td>4</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>    
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </body>
+</html>
