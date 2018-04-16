c6383bf4b173b13389fcf88c96981a6fab87dbe0
jeevan17 <jeevan17>
2018-04-16 11:44:50 +0530

    Timetable

--- Hod/timetable.php
+++ Hod/timetable.php
@@ -11,37 +11,94 @@
 	$hod_name = explode('_', $uname);
 
 	$days = array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+	$time_slot = array('09:40:00','10:30:00','11:20:00','12:10:00','01:35:00','02:25:00','03:15:00');
 	$staff = array();
 
-	$sql = "SELECT staff.FullName, timetable.YearandSem, bsp_code.Program, bsp_code.Branch, bsp_code.Section, timetable.Day, timetable.Timeslot, courses.CourseName FROM timetable NATURAL JOIN staff NATURAL JOIN bsp_code NATURAL JOIN courses WHERE staff.Department='$hod_name[1]' ORDER BY timetable.Day,timetable.Timeslot";
+	$sql = "SELECT staff.FullName, timetable.YearandSem, bsp_code.Program, bsp_code.Branch, bsp_code.Section, timetable.Day, timetable.Timeslot, courses.CourseName FROM timetable NATURAL JOIN staff NATURAL JOIN bsp_code NATURAL JOIN courses WHERE staff.Department='$hod_name[1]' GROUP BY staff.FullName ORDER BY timetable.Day,timetable.Timeslot ";
 	$data = array();
 	$retval = mysqli_query($conn, $sql);
 	while($row = mysqli_fetch_array($retval))
 	{
 		array_push($staff, $row['FullName']);
+		//array_push($days, $row['Day']);
+		//$data[$row['Day']][$row['FullName']] = '-';
 	}
-
-	foreach ($days as $day)
+	?>
+	<pre>
+	    <?php
+	        print_r($staff);
+	    ?>
+	</pre>
+	<?php
+	foreach ($staff as $name)
 	{
-		foreach ($staff as $name)
+		foreach ($time_slot as $time)
 		{
-			$data[$day][$name] = '-';
+			$data[$name][$time] = '-';
 		}
 	}
-	var_dump($data);
+	//var_dump($data);
 
 	$sql = "SELECT staff.FullName, timetable.YearandSem, bsp_code.Program, bsp_code.Branch, bsp_code.Section, timetable.Day, timetable.Timeslot, courses.CourseName FROM timetable NATURAL JOIN staff NATURAL JOIN bsp_code NATURAL JOIN courses WHERE staff.Department='$hod_name[1]' ORDER BY timetable.Day,timetable.Timeslot";
-	$data = array();
+	//$data = array();
 	$retval = mysqli_query($conn, $sql);
 	while($row = mysqli_fetch_array($retval))
 	{
-		array_push($data[$row['Day']][$row['FullName']], array($row['CourseName'],$row['YearandSem'],$row['Program'],$row['Branch'],$row['Section']));
+		// $a = array($row['CourseName'],$row['YearandSem'],$row['Program'],$row['Branch'],$row['Section']);
+		// $day = $row['Day'];
+		// $staff = $row['FullName'];
+		// array_push($data[$day][$staff], $a);
+		$data[$row['FullName']][$row['Timeslot']] = array($row['CourseName'],$row['YearandSem'],$row['Program'],$row['Branch'],$row['Section']);
 	}
 	echo "<br><hr>";
-	var_dump($data);	
-?>
-
-
+	?>
+	<table class='table table-bordered table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl'>
+		<thead>
+			<tr>
+				<th></th>
+				<th>09:40:00-10:30:00</th>
+				<th>10:30:00-11:20:00</th>
+				<th>11:20:00-12:10:00</th>
+				<th>12:10:00-01:00:00</th>
+				<th>01:35:00-02:25:00</th>
+				<th>02:25:00-03:15:00</th>
+				<th>03:15:00-04:05:00</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+				foreach ($data as $staff => $timeslot)
+				{
+					
+					echo "
+						<tr>
+							<th>$staff</th>
+					";
+				
+					
+					foreach ($timeslot as $time => $value)
+					{
+						echo "<h1>$staff</h1><pre>";
+						        var_dump($value);
+						echo "</pre>";
+						echo "<td>";
+						if ($value != '-')
+						{
+							echo "$value[0]<hr>
+								$value[2] $value[1] $value[3]-$value[4]
+							";
+						}
+						else
+						{
+							echo "<center style='font-size:  20px;'>$value</center>";
+						}
+						echo "</td>";
+					}
+					echo "</tr>";
+				}
+			?>
+		</tbody>
+	</table>
 </div>
 </div>
 		<script src='timetable/timetable.js'></script>
